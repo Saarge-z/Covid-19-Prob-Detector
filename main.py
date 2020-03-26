11d4c4dbade040c952ef6293daa557bfdcaf3deb
Saarge <razamohib01@gmail.com>
2020-03-26 19:33:41 +0530

    Basic Probabilty Generator

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import pickle
+# Create Flask app load app.config
+app = Flask(__name__)
+
+# open a file, where you data is stored and load it
+file = open('model.pkl', 'rb')
+clf = pickle.load(file)
+file.close()
+
+@app.route('/', methods=['GET', 'POST'])
+def screening():
+
+    if request.method == 'POST':
+        myDict = request.form
+        fever = int(myDict['fever'])
+        age = int(myDict['age'])
+        pain = int(myDict['pain'])
+        runnnyNose = int(myDict['runnyNose'])
+        diffBreath = int(myDict['diffBreath'])
+
+
+        # clf.predict([[100,1,21,1,1]]) # provides us with 0 or 1 (whether we have infec or not)
+        inputFeatures = [fever, pain, age, runnnyNose, diffBreath]
+        infProb = clf.predict_proba([inputFeatures])[0][1] # predicts infection prob. along with no infection prob.
+        return render_template('view.html', inf=round(infProb*100))
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ training.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def data_split(data, ratio):
+    shuffled = np.random.permutation(len(data))
+    test_set_size = int(len(data) * ratio)
+    
+    test_indices = shuffled[:test_set_size]
+    train_indices = shuffled[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+
+
+
+if __name__ == "__main__":
+    # Read data set
+    df = pd.read_csv('data.csv')
+    train, test = data_split(df, 0.2)
+
+    X_train = train[['fever','bodyPain','age','runnyNose','diffBreath']].to_numpy()
+    Y_test = test[['fever','bodyPain','age','runnyNose','diffBreath']].to_numpy()
+
+    Y_train = train[['infectionProb']].to_numpy().reshape(2413,)
+    Y_test = test[['infectionProb']].to_numpy().reshape(603,) #according to sklearn reshape(size of data,)
+
+    # training model
+    clf = LogisticRegression()
+    clf.fit(X_train, Y_train)
+
+
+    # open a file, where you ant to store the data
+    file = open('model.pkl', 'wb')
+
+    # dump information to that file
+    pickle.dump(clf, file)
+    file.close()
