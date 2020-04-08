0f8e2e600eb77443faee517fabdc74986f5981e8
Saarge <razamohib01@gmail.com>
2020-04-08 20:35:54 +0530

    Improved Accuracy

--- training.py
+++ training.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import pickle
+from sklearn.preprocessing import StandardScaler
 
 def data_split(data, ratio):
     shuffled = np.random.permutation(len(data))
@@ -21,10 +22,27 @@ if __name__ == "__main__":
     train, test = data_split(df, 0.2)
 
     X_train = train[['fever','bodyPain','age','runnyNose','diffBreath']].to_numpy()
-    Y_test = test[['fever','bodyPain','age','runnyNose','diffBreath']].to_numpy()
+    X_test = test[['fever','bodyPain','age','runnyNose','diffBreath']].to_numpy()
 
     Y_train = train[['infectionProb']].to_numpy().reshape(2413,)
     Y_test = test[['infectionProb']].to_numpy().reshape(603,) #according to sklearn reshape(size of data,)
+
+
+    # Preparations for Feature Scaling
+    Xtn,Xtn2 = X_train[:,[0,2]], X_train[:,[1,3,4]]
+    Xts,Xts2 = X_test[:,[0,2]], X_test[:,[1,3,4]]
+
+
+    # Feature Scaling
+    sc_X = StandardScaler()
+    X_train = sc_X.fit_transform(Xtn)
+    X_test = sc_X.fit_transform(Xts)
+
+    X_train = np.append(X_train, Xtn2, 1)
+    X_test = np.append(X_test, Xts2, 1)
+    # After feature Scalling Index is
+    # Fever, Age, bodyPain, runnnyNose, diffBreath
+
 
     # training model
     clf = LogisticRegression()
